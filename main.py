fc65d955d0710144c280016708e403bcb8973f3a
dag157 <dag157@pitt.edu>
2019-10-15 15:29:10 -0400

    added parser

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 class PracticeTestMaker:
 
-    def createTest(self):
-        print('hi')
+    def parse_input(self, input_file):
+        filepath = input_file
+        questions = []
+        with open(filepath) as fp:
+            line = fp.readline()
+            while line:
+                line = fp.readline()
+
+        return questions
+
+
+    def createTest(self, question_file, amt_of_qs):
+        question_bank = self.parse_input(question_file)
 
 
 if __name__ == '__main__':
     p = PracticeTestMaker()
-    p.createTest()
+    p.createTest('cv_questions.txt',5)
